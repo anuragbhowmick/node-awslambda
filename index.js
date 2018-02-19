16c3e56197cdb9384e4105108e725b9069f98a8a
anuragbhowmick <anurag.bhowmick@vizury.com>
2018-02-19 21:48:46 +0530

    passing json object as result

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 var webPush = require('web-push');
 
-function updateResult(resultJSON,notificationCount, notification, message, callback) {
-	var cookie = notification.cookie;
-    var advId = notification.advId;
-    var analyzeAdvId = notification.AnalyzeAdvId;
-    var bannerId = notification.bannerid;
+function updateResult(resultJSON, impressionId, message) {
+	// var cookie = notification.cookie;
+ //    var advId = notification.advId;
+ //    var analyzeAdvId = notification.AnalyzeAdvId;
+ //    var bannerId = notification.bannerid;
     var imprId = notification.payload.notificationid;
-    resultJSON.push({
-        "cookie":cookie,
-        "message":message,
-        "advId":advId,
-        "analyzeAdvId":analyzeAdvId,
-        "bannerId":bannerId,
-        "imprId":imprId
-    });
-    console.log("result length " + resultJSON.length);
+    resultJSON.imprId = message;
+    // resultJSON.push({
+    //     "cookie":cookie,
+    //     "message":message,
+    //     "advId":advId,
+    //     "analyzeAdvId":analyzeAdvId,
+    //     "bannerId":bannerId,
+    //     "imprId":imprId
+    // });
+    // console.log("result length " + resultJSON.length);
     // if(resultJSON.length >= notificationCount) {
     //     callback(null,resultJSON);
     // }
@@ -32,26 +33,28 @@ exports.handler = (event, context, callback) => {
     //         'Content-Type': 'application/json',
     //     },
     // });
-    var done = null;
-    var notificationArray = JSON.parse(event.body);
-    var notificationCount = notificationArray.length;
 
-   var resultJSON = JSON.parse("[]");
+    var notificationArray = JSON.parse(event.body);
+
+   var resultJSON = JSON.parse("{}");
    var promises = [];
-    for(var i = 0 ; i < notificationCount ; i++) {
-        let notification = notificationArray[i];
+    for(var i = 0 ; i < notificationArray.length ; i++) {
+        var notification = notificationArray[i];
         var endpoint = notification.endpoint;
         var gcmkey = notification.gcmkey;
         var auth = notification.auth;
         var key = notification.key;
         // if(i != 0) {
-        // 	cookie += i;
+        // 	notification.cookie = 'viz_my_'+i;
         // 	endpoint += Math.floor((Math.random() * 1000) + 1);
         // 	auth += Math.floor((Math.random() * 1000) + 1);
         // 	key += Math.floor((Math.random() * 1000) + 1);
+        //     payload.notificationid = "impr_" + i;
         // }
         var payload = notification.payload;
         var msg_expiry_ttl = notification.payload.time_to_live ? notification.payload.time_to_live : 2419200;
+
+        let imprId = payload.notificationid;
 
         var promise = webPush.sendNotification(endpoint, {
             payload: JSON.stringify(payload),
@@ -61,15 +64,15 @@ exports.handler = (event, context, callback) => {
             TTL: msg_expiry_ttl
         }).then(function(event) {
             console.log("Success: " + notification.cookie);
-            updateResult(resultJSON,notificationCount,notification,"SUCCESS",done);
+            resultJSON[imprId] = "SUCCESS";
         }).catch(function(e) {
-            console.log("Error: " + notification.cookie + " Message: " + JSON.stringify(e));
-            updateResult(resultJSON,notificationCount,notification,JSON.stringify(e.body),done);
+            // console.log("Error: " + notification.cookie + " Message: " + JSON.stringify(e));
+            resultJSON[imprId] = JSON.stringify(e.body);
         });
         promises.push(promise);
     }
     Promise.all(promises).then(function(values) {
-    	console.log("Finished with all calls " + values);
+    	// console.log("Finished with all calls " + JSON.stringify(promises));
     	callback(null, {
         	statusCode: '200',
         	body: JSON.stringify(resultJSON),
